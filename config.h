b3361d74b96589905eeaa05191f825a5b67efaba
vaproh <vaproh@proton.me>
2024-06-18 09:43:29 +0530

    added icon

--- config.h
+++ config.h
@@ -124,7 +124,7 @@ static const char *colorname[] = {
 	"#e5e9f0", /* white   */
 	
 	/* 8 bright colors */
-	"#4c566a", /* black   */
+	"#C0D6E4", /* black   */
 	"#bf616a", /* red     */
 	"#a3be8c", /* green   */
 	"#ebcb8b", /* yellow  */
@@ -138,8 +138,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
+	"#2e3440", /* 258 -> bg */
+	"#d8dee9", /* 259 -> fg */
 };
 
 
