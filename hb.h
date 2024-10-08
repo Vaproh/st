a76860ccc087c32d41667a13b8693a8a3b5e29f9
Vaproh <contact@vaproh.xyz>
2024-10-08 10:30:15 +0530

    new stuff added 😁

--- hb.h
+++ hb.h
@@ -1,7 +1,1 @@
-#include <X11/Xft/Xft.h>
-#include <hb.h>
-#include <hb-ft.h>
-
-void hbunloadfonts();
-void hbtransform(XftGlyphFontSpec *, const Glyph *, size_t, int, int);
-
+../../../.dotfiles/.local/src/st/hb.h
